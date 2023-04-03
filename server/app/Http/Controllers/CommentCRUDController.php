b2e3926a628d3e59aa54e84a69e671c09b8af17b
Sergei Martynov <Eldar231109@gmail.com>
2023-04-04 00:49:34 +0300

    Server

---
+++ server/app/Http/Controllers/CommentCRUDController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentCRUDController extends Controller
+{
+    public function index()
+    {
+        $data = Comment::all();
+        return ($data);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'text' => 'required',
+        ]);
+        $comment = new Comment;
+        $comment->name = $request->name;
+        $comment->text = $request->text;
+        $comment->save();
+        return $comment;
+    }
+
+    public function destroy(Comment $id)
+    {
+        $id->delete();
+        return Comment::all();
+    }
+}
